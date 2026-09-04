d2c49ebe98894cd3ae0ecb81c1b05f0fa08edc8c
drynwd2rzd-beep <drynwd2rzd@privaterelay.appleid.com>
2026-09-04 16:06:25 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,1 @@
-const CACHE='oneday-pwa-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
- if(e.request.method!=='GET') return;
- e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{
-   const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
- }).catch(()=>caches.match('./index.html'))));
-});
+const CACHE='oneday-pwa-v2-home';const A=['./','./index.html','./manifest.json','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(x=>x||caches.match('./index.html'))))});
